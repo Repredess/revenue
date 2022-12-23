8de1b5113d79e7f239cc2fb0bf9a1142caac0b9c
Repredess <vverkir@gmail.com>
2022-12-23 19:24:27 +0300

    change to class

--- main.py
+++ main.py
@@ -1,15 +1,58 @@
-pull = dict(Наличные_на_утро=0, Наличные_на_вечер=0, Безналичный_рассчет=0, Расход=0, Инкасация=0)
+import datetime as dt
 
 
-def revenue(pull):
-    for key, value in pull.items():
-        print(key.replace("_", " ") + ":", end=" ")
-        pull[key] += int(input())
+class Revenue:
+    PULL = dict(Наличные_на_утро=0, Наличные_на_вечер=0, Безналичный_рассчет=0, Расход=0, Инкасация=0)
 
-    rev_total = pull["Наличные_на_вечер"] + pull["Безналичный_рассчет"] + pull["Расход"] + pull["Инкасация"] - pull["Наличные_на_утро"]
+    # startapp
+    def __init__(self):
+        self.file = 'data.txt'
+        self.head = self.read_head(self.file)
+        self.date = str(dt.date.today())
+        # self.line = ''
 
-    return print("Выручка: " + str(rev_total))
+    # take data
+    def take_data(self):
+        data = [self.date]
+
+        for key, value in self.PULL.items():
+            print(key.replace("_", " ") + ":", end=" ")
+            self.PULL[key] = input()
+            data.append(self.PULL[key])
+
+        return data
+
+    # set data
+    def set_data(self):
+
+        with open(self.file, 'r+', encoding='utf-8') as f:
+            count = 0
+            data = self.take_data()
+
+            for line in f.readlines():
+                count += 1
+
+            data.insert(0, str(count))
+            print("  ".join(data), file=f)
+
+        return data
+
+    # get data
+    def get_data(self):
+        pass
+
+    @classmethod
+    def read_head(cls, file):
+        with open(file, encoding='utf-8') as f:
+            return f.readline().strip().split()
+
+    @classmethod
+    def __check_value(cls, x):
+        return type(x) in (int, float)
+
+    def get_head(self):
+        return self.head
 
 
-revenue(pull)
-
+pt = Revenue()
+print(pt.set_data())
