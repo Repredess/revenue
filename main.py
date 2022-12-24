689e6965b428f6258e240636a89bd512370d36d9
Repredess <vverkir@gmail.com>
2022-12-24 14:31:43 +0300

    fix line problems, db

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class Revenue:
     # startapp
     def __init__(self):
         self.file = 'data.txt'
+        self.rev = 'rev.txt'
         self.head = self.read_head(self.file)
         self.date = str(dt.date.today())
         # self.line = ''
@@ -26,20 +27,49 @@ class Revenue:
     def set_data(self):
 
         with open(self.file, 'r+', encoding='utf-8') as f:
-            count = 0
+            count = len(f.readlines())
+            if count < 2:
+                self.fix_line(f)
             data = self.take_data()
-
-            for line in f.readlines():
-                count += 1
 
             data.insert(0, str(count))
             print("  ".join(data), file=f)
 
-        return data
+        return data, count
+
+    def get_revenue(self):
+        box, id_line = self.set_data()
+        evening_cash = self.get_morning_cash(self.file)
+        sum_r = 0
+        for i in box[3:]:
+            sum_r += int(i)
+
+        revenue = sum_r - evening_cash
+
+        with open(self.rev, 'a+', encoding='utf-8') as f:
+            if id_line < 2:
+                self.fix_line(f)
+            print('  '.join([str(id_line), self.date, str(revenue)]), file=f)
+
+        return revenue
 
     # get data
     def get_data(self):
         pass
+
+    @classmethod
+    def fix_line(cls, file):
+
+        return print('', file=file)
+
+    @classmethod
+    def get_morning_cash(cls, file):
+        with open(file) as f:
+            get_ec = f.readlines()
+            get_ec = get_ec[-1].strip().split()
+
+
+        return int(get_ec[-5])
 
     @classmethod
     def read_head(cls, file):
@@ -55,4 +85,5 @@ class Revenue:
 
 
 pt = Revenue()
-print(pt.set_data())
+# print(pt.set_data())
+print(pt.get_revenue())
